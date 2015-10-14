90639f749cc058f48b91a3f7e5a026a1d40b0685
Luke Langfield <lukelangfield@ltlcreation.com>
2015-10-14 21:00:30 +0100

    Initial Commit

---
+++ Newletter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Newletter
+//
+//  Created by Luke Langfield on 14/10/2015.
+//  Copyright © 2015 ltlcreation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
